0a2195bffeffc070dfca629921e0ae0870869477
cmoonth <cmoonth47@gmail.com>
2024-03-07 14:16:39 +0900

    24.03.07 Add OrderWebPage 1. resource - add bootstrap - add addForm.html - add editForm.html - add item.html - add items.html (Test)
    2. main
    - add domain/item/ItemRepository class
    - add domain/item/Item class
    - add web/basic/BasicItemController class

--- item-service/src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ item-service/src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -1,2 +1,45 @@
-package hello.itemservice.domain.item;public class ItemRepository {
+package hello.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+
+    private static final Map<Long, Item> store = new HashMap<>();   //static
+    private static long sequence = 0L;  //static
+
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id) {
+        return store.get(id);
+
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long itemId, Item updateParam) {
+        Item findItem = findById(itemId);
+        //getter, setter를 이용해서 값을 설정하고 전달하는 방법은 명확하지 않음 => 실무에서 잘 쓰이지 않는다.
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+        //클래스를 만들어서 객체를 생성해 사용해줄 수 있음 => DTO를 사용해서 명확하게 사용해주는 것이 좋음
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+
+
 }
